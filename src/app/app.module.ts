6ca034c5c97fd338a4075dda56b42c524e21997f
evrlast <dmytro.chaschin@gmail.com>
2022-08-06 23:09:51 +0300

    Init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EngineComponent } from './engine/engine.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EngineComponent
   ],
   imports: [
     BrowserModule
